e8e0b06ea34943c784f1e2dea4b03fa5df36742f
martin <martin.papanek@gmail.com>
2018-08-19 01:56:14 +0200

    improving navigation and working on data entry and visualization

--- lib/data_base.dart
+++ lib/data_base.dart
@@ -34,6 +34,40 @@ class BismuthDbConnection {
   }
   BismuthDbConnection(this._db);
 
+  Future<void> initTestData() async {
+    Group g1 = new Group(name: "testG", order: 1);
+    Group g2 = new Group(name: "gunit", order: 2);
+
+    await putGroup(g1);
+    await putGroup(g2);
+
+    Track t1 = new Track(name: "weight", group: g1, units: "kg", hasMovingAverage: true, movingAvgDays: 10);
+    Track t2 = new Track(name: "test", group: g2, units: "hops");
+    Track t3 = new Track(name: "test2", group: g2, units: "hops");
+
+    await putTrack(t1);
+    await putTrack(t2);
+    await putTrack(t3);
+
+    TrackData td1 = new TrackData(track: t1, time: new DateTime(2017, 10, 18), value: 10);
+    TrackData td2 = new TrackData(track: t1, time: new DateTime(2017, 10, 25), value: 100);
+    TrackData td3 = new TrackData(track: t1, time: new DateTime(2017, 11, 13), value: 50);
+
+    TrackData td4 = new TrackData(track: t2, time: new DateTime(2018, 1, 10), value: 50);
+    TrackData td5 = new TrackData(track: t2, time: new DateTime(2018, 1, 15), value: 60);
+    TrackData td6 = new TrackData(track: t2, time: new DateTime(2018, 2, 1), value: 70);
+    TrackData td7 = new TrackData(track: t2, time: new DateTime(2018, 2, 16), value: 65);
+
+    await putTrackData(td1);
+    await putTrackData(td2);
+    await putTrackData(td3);
+
+    await putTrackData(td4);
+    await putTrackData(td5);
+    await putTrackData(td6);
+    await putTrackData(td7);
+  }
+
   Future<void> putGroup(Group group) async {
     final store = _db.getStore(_GROUP_STRORE_KEY);
     await store.put(json.encode(group), group.name);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'dart:async';
+import 'package:bismuth/model/group.dart';
 import 'package:bismuth/model/track.dart';
+import 'package:bismuth/model/track_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'data_base.dart';
 import 'charts.dart';
 import 'package:page_view_indicator/page_view_indicator.dart';
+
+Group g1 = new Group(name: "gunit", order: 2);
+Track t1 = new Track(name: "test2", group: g1, units: "hops");
+List<TrackData> tds = [
+  new TrackData(track: t1, time: new DateTime(2018, 1, 1), value: 10),
+  new TrackData(track: t1, time: new DateTime(2018, 1, 15), value: 10)
+];
 
 Future<void> main() async {
   final dbConnection = await BismuthDbConnection.openConnection();
 
   //await dbConnection.initHello();
+  //await dbConnection.initTestData();
+
+  //debugPaintSizeEnabled = true;
 
   runApp(new BismuthApp());
 }
@@ -19,8 +33,10 @@ typedef void Action(BuildContext context);
 class TrackPage extends StatelessWidget {
   final ChartBuilder chartBuilder;
   final Track track;
+  final List<TrackData> trackData;
+  final TextStyle headerTextStyle = new TextStyle(fontWeight: FontWeight.bold);
 
-  TrackPage({this.chartBuilder, this.track});
+  TrackPage({this.chartBuilder, this.track, this.trackData = const []});
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +48,77 @@ class TrackPage extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new Container(
-              child: chartBuilder(track),
-              height: windowSize.height * 0.8,
-              width: windowSize.width * 0.8),
+          new Container(child: chartBuilder(track), height: windowSize.height * 0.65, width: windowSize.width * 0.8),
+          new Expanded(
+              child: new Container(
+                  width: windowSize.width * 0.5,
+                  child: new TrackDataListView(trackData: trackData, headerTextStyle: headerTextStyle)))
           //new Text("hello"),
         ],
       ),
+    );
+  }
+}
+
+class TrackDataListView extends StatelessWidget {
+  const TrackDataListView({
+    Key key,
+    @required this.trackData,
+    @required this.headerTextStyle,
+  }) : super(key: key);
+
+  final List<TrackData> trackData;
+  final TextStyle headerTextStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+        itemCount: trackData.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return new TrackListViewHeaderRow(headerTextStyle: headerTextStyle);
+          }
+
+          final td = trackData[index - 1];
+          return new TrackListViewDataRow(td: td);
+        });
+  }
+}
+
+class TrackListViewDataRow extends StatelessWidget {
+  const TrackListViewDataRow({
+    Key key,
+    @required this.td,
+  }) : super(key: key);
+
+  final TrackData td;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Text(td.time),
+        new Expanded(child: new Text(td.value.toString(), textAlign: TextAlign.right))
+      ],
+    );
+  }
+}
+
+class TrackListViewHeaderRow extends StatelessWidget {
+  const TrackListViewHeaderRow({
+    Key key,
+    @required this.headerTextStyle,
+  }) : super(key: key);
+
+  final TextStyle headerTextStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Text("Date", style: headerTextStyle),
+        new Expanded(child: new Text("Value", style: headerTextStyle, textAlign: TextAlign.right))
+      ],
     );
   }
 }
@@ -57,26 +137,55 @@ class Choice {
   }
 }
 
+MaterialPageRoute<void> createSaveTrackRoute() {
+  return new MaterialPageRoute<void>(builder: (BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: const Text("New Track")),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.save),
+        onPressed: () async {
+          await SystemChannels.textInput.invokeMethod('TextInput.hide');
+          Navigator.of(context).pop();
+        },
+      ),
+      body: new Column(children: <Widget>[
+        new Expanded(
+            child: TextField(
+          decoration: InputDecoration(hintText: 'Name'),
+        ))
+      ]),
+    );
+  });
+}
+
+MaterialPageRoute<void> createSaveTrackDataRoute() {
+  return new MaterialPageRoute<void>(builder: (BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: const Text("Track Data Entry")),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.save),
+        onPressed: () async {
+          // hide the software keyboard
+          await SystemChannels.textInput.invokeMethod('TextInput.hide');
+          Navigator.of(context).pop();
+        },
+      ),
+      body: new Column(children: <Widget>[
+        new Expanded(
+            child: TextField(
+              decoration: InputDecoration(hintText: 'Value'),
+            ))
+      ]),
+    );
+  });
+}
+
 List<Choice> choices = <Choice>[
   Choice(
       title: 'New Track',
       icon: Icons.add,
       action: (context) {
-        Navigator
-            .of(context)
-            .push(new MaterialPageRoute<void>(builder: (BuildContext context) {
-          return new Scaffold(
-            appBar: new AppBar(title: const Text("New Track")),
-            body: new Stack(
-                alignment: FractionalOffset.bottomCenter,
-                children: <Widget>[
-                  new Text("hmm"),
-                  new IconButton(
-                      icon: Icon(Icons.save),
-                      onPressed: () => Navigator.of(context).pop())
-                ]),
-          );
-        }));
+        Navigator.of(context).push(createSaveTrackRoute());
       }),
   const Choice(title: 'New Group', icon: Icons.add_shopping_cart),
   const Choice(title: 'Delete Track', icon: Icons.remove),
@@ -102,6 +211,7 @@ class TracksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        resizeToAvoidBottomPadding: false, //this prevents this widget from resizing due to software keyboard (which would cause problems)
         appBar: new AppBar(
           title: Text('Bismuth'),
           actions: <Widget>[
@@ -133,28 +243,30 @@ class TracksPage extends StatelessWidget {
             ),
           ],
         ),
-        body: new Stack(
-            alignment: FractionalOffset.bottomCenter,
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).push(createSaveTrackDataRoute());
+          },
+        ),
+        body: new Column(children: <Widget>[
+          new Expanded(
+              child: new PageView(
+            onPageChanged: (index) => pageIndexNotifier.value = index,
             children: <Widget>[
-              new PageView(
-                onPageChanged: (index) => pageIndexNotifier.value = index,
-                children: <Widget>[
-                  new TrackPage(
-                      chartBuilder: (track) =>
-                          SimpleTimeSeriesChart.withSampleData(),
-                      track: null),
-                  new TrackPage(
-                      chartBuilder: (track) =>
-                          SimpleTimeSeriesChart.withSampleData2(),
-                      track: null),
-                ],
+              new TrackPage(
+                chartBuilder: (track) => SimpleTimeSeriesChart.withSampleData(),
+                track: t1,
+                trackData: tds,
               ),
-              _createPageIndicator(pageIndexNotifier, 2)
-            ]));
+              new TrackPage(chartBuilder: (track) => SimpleTimeSeriesChart.withSampleData(), track: t1),
+            ],
+          )),
+          _createPageIndicator(pageIndexNotifier, 2)
+        ]));
   }
 
-  Widget _createPageIndicator(
-      ValueNotifier<int> pageIndexNotifier, int length) {
+  Widget _createPageIndicator(ValueNotifier<int> pageIndexNotifier, int length) {
     return PageViewIndicator(
       pageIndexNotifier: pageIndexNotifier,
       length: length,
